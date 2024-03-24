4f09424be2420ff11c70a5f45855a07d713feaa0
shahinexy <shahinexy@gmail.com>
2024-03-24 23:09:55 +0600

    set data theme

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 
 
@@ -8,6 +9,23 @@ const Header = () => {
                 <li><NavLink to={'/bookmarks'}>Bookmarks</NavLink></li>
                 
     </>
+
+    const [theme, setTheme] = useState('light')
+    const handleThem = e => {
+        if(e.target.checked){
+            setTheme('synthwave')
+        }
+        else{
+            setTheme('light')
+        }
+    }
+    console.log(theme);
+
+    useEffect(()=>{
+        localStorage.setItem('theme', theme);
+        const storedTheme = localStorage.getItem('theme')
+        document.querySelector('html').setAttribute('data-theme', storedTheme)
+    },[theme])
 
     return (
         <div>
@@ -33,7 +51,7 @@ const Header = () => {
                     <div className="ml-3">
                     <label className="swap swap-rotate">  
                         {/* this hidden checkbox controls the state */}
-                        <input type="checkbox" className="theme-controller" value="synthwave" />
+                        <input onChange={handleThem} type="checkbox" className="theme-controller" value="synthwave" />
                         
                         {/* sun icon */}
                         <svg className="swap-off fill-current w-10 h-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z"/></svg>
